78d9bc48ef06cfc58809af79dc8b019a438223de
Jeff <jeff.doyle@foxmail.com>
2022-04-06 21:14:36 +0800

    Finish function.ts

--- src/function.ts
+++ src/function.ts
@@ -31,3 +31,19 @@ function addFunction7(x: number, ...rest: number[]) {
 }
 
 console.log(addFunction7(1, 2, 3, 4, 5)); // 15
+
+function AddFunction8(...rest: number[]): number;
+function AddFunction8(...rest: string[]): string;
+function AddFunction8(...rest: any[]): any {
+    let first = rest[0];
+    if (typeof first === 'string') {
+        return rest.join('');
+    }
+
+    if (typeof first === 'number') {
+        return rest.reduce((pre, cur) => pre + cur);
+    }
+}
+
+console.log(AddFunction8(1, 2, 3)); // 6
+console.log(AddFunction8('a', 'b', 'c')); // 'abc'
